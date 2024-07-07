b1ed0f9096b6eac9f5efa2b86c93bfbd1e80b8d7
ktsvilii <kos.tsvilii@gmail.com>
2024-07-07 15:39:04 +0300

    fixing issue with socketio

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:5000', {
+      const socket = io('https://realtimechat-7idm.onrender.com', {
         query: {
           userId: authUser._id,
         },
@@ -37,9 +37,5 @@ export const SocketContextProvider = ({ children }) => {
     }
   }, [authUser]);
 
-  return (
-    <SocketContext.Provider value={{ socket, onlineUsers }}>
-      {children}
-    </SocketContext.Provider>
-  );
+  return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
